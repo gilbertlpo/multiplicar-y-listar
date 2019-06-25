bac1c627ad5ca504a380a75e1ad5c04b1671dc13
gilbertlpo <gcabanascantero@gmail.com>
2019-06-24 21:05:52 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//requireds
+const { crearArchivo } = require('./multiplicar/multiplicar');
+
+
+let base = '10';
+
+
+  crearArchivo(base)
+  .then( archivo => console.log(`Archivo creado: ${ archivo }`))
+  .catch(e => console.log(e));
